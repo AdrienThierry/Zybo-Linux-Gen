b0ab6a951c2f83a61d40486b98de6a2d1ee5c2ff
Adrien Thierry <adrienthierry2@gmail.com>
2017-01-03 15:25:21 +0100

    Added a script to build Linux kernel

---
+++ linux_sources/build_kernel.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# ------------------------------------------------------------
+# build_kernel.sh
+# Adrien THIERRY
+# 
+# Build Linux kernel from Xilinx git repo
+# ------------------------------------------------------------
+
+# Global variables
+options_list="
+OPTIONS :
+-t [path] | --toolchain [path]	(optional) Vivado toolchain location
+				default : (HOME dir)/Xilinx/Vivado/(version)/
+"
+
+# Parse arguments
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+	-t|--toolchain)
+	vivado_path=$2
+	shift
+	;;
+	*)
+	echo "$options_list" # unknown option
+	exit
+	;;
+esac
+shift # past argument or value
+done
+# END parse arguments
+
+# Source Xilinx tools
+source ../source_toolchain.sh $vivado_path
+
+# Clone Xilinx Linux kernel repo
+git clone https://github.com/Xilinx/linux-xlnx.git
+
+# Use ARM cross compiler
+export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+
+cd linux-xlnx
+
+make ARCH=arm xilinx_zynq_defconfig
+make ARCH=arm UIMAGE_LOADADDR=0x8000 uImage
